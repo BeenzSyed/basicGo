7cc75c6b1de22ffef0e3e43aa81a674efd344d87
Sabeen Syed <sabeen.syed@rackspace.com>
2017-06-07 20:18:58 -0500

    Update CheckUser

--- models/models.go
+++ models/models.go
@@ -1,6 +1,19 @@
 package models
 
-import "github.com/astaxie/beego/orm"
+import (
+	"fmt"
+
+	"github.com/astaxie/beego/orm"
+)
+
+type Users struct {
+	Id       int
+	username string
+}
+
+func init() {
+	orm.RegisterModel(new(Users))
+}
 
 func AddUser(username string) error {
 	o := orm.NewOrm()
@@ -15,21 +28,16 @@ func CheckUser(username string) error {
 	o := orm.NewOrm()
 	o.Using("default")
 
-	err := o.Raw("SELECT EXISTS(SELECT * FROM users WHERE username = ?)", username).QueryRow()
+	users := Users{Id: 1}
 
-	// var p []byte
-	// err := db.QueryRow("SELECT data FROM message WHERE data->>'id'=$1", id).Scan(&p)
+	err := o.Read(&users)
 
+	if err == orm.ErrNoRows {
+		fmt.Println("No result found.")
+	} else if err == orm.ErrMissPK {
+		fmt.Println("No primary key found.")
+	} else {
+		fmt.Printf("Results: %v\n", users.username)
+	}
 	return err
 }
-
-// func rowExists(query string, args ...interface{}) bool {
-//
-// 	var exists bool
-// 	query = fmt.Sprintf("SELECT exists (%s)", query)
-// 	err := db.QueryRow(query, args...).Scan(&exists)
-// 	if err != nil && err != sql.ErrNoRows {
-// 		glog.Fatalf("error checking if row exists '%s' %v", args, err)
-// 	}
-// 	return exists
-// }
